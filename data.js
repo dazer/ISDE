e9916ae2da90e4762f20f586e1bb8a0c884134da
néstor <nextorniquete@gmail.com>
2019-05-28 22:39:13 +0200

    corridor

--- data.js
+++ data.js
@@ -1,8 +1,35 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-panorama",
-      "name": "panorama",
+      "id": "0-aula",
+      "name": "aula",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pasillo",
+      "name": "pasillo",
       "levels": [
         {
           "tileSize": 256,
